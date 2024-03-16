f29b8964d2ff091290ee9806686d98b8b0ada827
Phuc Le <phuc.bcle@successsoftware.global>
2024-03-16 10:50:32 +0700

    Fix issue Wordpress plugin review

--- public/views/footer.php
+++ public/views/footer.php
@@ -7,7 +7,7 @@
     window.__be = window.__be || {};
     (function () {
         var be = document.createElement('script'); be.type = 'text/javascript'; be.async = true;
-        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + '<?php echo esc_html(GENQIZCHATBOT_CHATBOT_URL); ?>/chat-widget.js?token=<?php echo esc_html(GENQIZCHATBOT_Public::get_instance()->data_token_id); ?>&inbox=<?= esc_html(GENQIZCHATBOT_Public::get_instance()->data_inbox_id); ?>'
+        be.src = ('https:' == document.location.protocol ? 'https://' : 'http://') + '<?php echo esc_html(GENQIZCHATBOT_CHATBOT_URL); ?>/chat-widget.js?token=<?php echo esc_html(GENQIZCHATBOT_Public::get_instance()->data_token_id); ?>&inbox=<?php echo esc_html(GENQIZCHATBOT_Public::get_instance()->data_inbox_id); ?>'
         var s = document.getElementsByTagName('script')[0];
         s.parentNode.insertBefore(be, s);
     })();
